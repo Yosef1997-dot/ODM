531a6100a84ab2ae844ec4f5f2a6b961d8994ec6
yosefKenig <yosefkenig@icloud.com>
2022-07-04 11:06:50 +0300

    changing the arrows to be the same height as the differnce in income

--- src/components/Main/Right/Comparision/index2.jsx
+++ src/components/Main/Right/Comparision/index2.jsx
@@ -1,9 +1,9 @@
 import styles from "./style.module.css"
 import axios from "axios";
 import { useState, useEffect } from "react";
-// import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
-// import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import Arrow from '../../../Arrow'
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
 export default function Comparision({ idsCheckedList }) {
 
     const [comparedUsers, setComparedUsers] = useState({})
@@ -11,6 +11,7 @@ export default function Comparision({ idsCheckedList }) {
     useEffect(() => {
         axios.get(`http://localhost:5003/${idsCheckedList[0]}/${idsCheckedList[1]}`)
             .then(res => setComparedUsers(res.data))
+            setTimeout(()=>console.log('loading'), 3000)
     },[])
 
     return <div className={styles.right}>
@@ -27,8 +28,7 @@ export default function Comparision({ idsCheckedList }) {
                     <div className={styles.head} style={{ backgroundImage: ` linear-gradient(to bottom, white ${(comparedUsers[0]?.percentage) ? comparedUsers[0]?.percentage : 0}, rgba(0,0,0,0) ${(comparedUsers[0]?.percentage) ? comparedUsers[0].percentage : 0})`}} ></div>
                     <div className={styles.name}>{comparedUsers[0]?.name}</div>
                     <div className={styles.total}>${comparedUsers[0]?.salary.toLocaleString('en-US')}</div>
-                    {/* <div className={styles.difference}>{(comparedUsers[0]?.difference < 0) ? `-` : ``}${Math.abs(comparedUsers[0]?.difference).toLocaleString('en-US')}{(comparedUsers[0]?.difference < 0) ? <ArrowDropDownIcon sx={{ color: '#2F80ED' }} /> : <ArrowDropUpIcon sx={{ color: '#2F80ED' }} />}</div> */}
-                    <div className={styles.difference}>{(comparedUsers[0]?.difference < 0) ? `-` : ``}${Math.abs(comparedUsers[0]?.difference).toLocaleString('en-US')}{(comparedUsers[0]?.difference < 0) ? <Arrow arrowColor={'#2F80ED'} direction={'down'} /> : <Arrow arrowColor={'#2F80ED'} direction={'up'}/>}</div>
+                    <div className={styles.difference}>{(comparedUsers[0]?.difference < 0) ? `-` : ``}${Math.abs(comparedUsers[0]?.difference).toLocaleString('en-US')}{(comparedUsers[0]?.difference < 0) ? <ArrowDropDownIcon sx={{ color: '#2F80ED' }} /> : <ArrowDropUpIcon sx={{ color: '#2F80ED' }} />}</div>
                 </div>
 
                 <div className={styles.middle}>
@@ -43,9 +43,7 @@ export default function Comparision({ idsCheckedList }) {
                     <div className={styles.head} style={{ backgroundImage: ` linear-gradient(to bottom, white ${(comparedUsers[1]?.percentage) ? comparedUsers[1]?.percentage : 0}, rgba(0,0,0,0) ${(comparedUsers[1]?.percentage) ? comparedUsers[1].percentage : 0})` }} ></div>
                     <div className={styles.name}>{comparedUsers[1]?.name}</div>
                     <div className={styles.total}>${comparedUsers[1]?.salary.toLocaleString('en-US')}</div>
-                    {/* <div className={styles.difference}>{(comparedUsers[1]?.difference < 0) ? `-` : ``}${Math.abs(comparedUsers[1]?.difference).toLocaleString('en-US')}{(comparedUsers[1]?.difference < 0) ? <ArrowDropDownIcon sx={{ color: '#F2994A' }} /> : <ArrowDropUpIcon sx={{ color: '#F2994A' }} />}</div> */}
-                    <div className={styles.difference}>{(comparedUsers[1]?.difference < 0) ? `-` : ``}${Math.abs(comparedUsers[1]?.difference).toLocaleString('en-US')}{(comparedUsers[1]?.difference < 0) ? <Arrow arrowColor={'#F2994A'} direction={'down'} /> : <Arrow arrowColor={'#F2994A'} direction={'up'}/>}</div>
-
+                    <div className={styles.difference}>{(comparedUsers[1]?.difference < 0) ? `-` : ``}${Math.abs(comparedUsers[1]?.difference).toLocaleString('en-US')}{(comparedUsers[1]?.difference < 0) ? <ArrowDropDownIcon sx={{ color: '#F2994A' }} /> : <ArrowDropUpIcon sx={{ color: '#F2994A' }} />}</div>
                 </div>
 
             </div>
