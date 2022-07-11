68a23afd589f3648cbad3c8026eaa6b037ae263d
yosefKenig <yosefkenig@icloud.com>
2022-07-11 14:41:39 +0300

    adding instruction for locally running and changing the variable user to employee

--- src/components/Comparison/index.jsx
+++ src/components/Comparison/index.jsx
@@ -4,11 +4,11 @@ import { useState, useEffect } from "react";
 import Arrow from '../Arrow'
 export default function Comparison({ idsCheckedList }) {
 
-    const [comparedUsers, setComparedUsers] = useState({})
+    const [compareEmployees, setCompareEmployees] = useState({})
 
     useEffect(() => {
         axios.get(`http://localhost:5000/${idsCheckedList[0]}/${idsCheckedList[1]}`)
-            .then(res => setComparedUsers(res.data))
+            .then(res => setCompareEmployees(res.data))
     }, [])
 
     return (
@@ -23,10 +23,10 @@ export default function Comparison({ idsCheckedList }) {
 
                 <div className={styles.comparison}>
                     <div className={`${styles.person} ${styles.one}`}>
-                        <div className={styles.head} style={{ backgroundImage: ` linear-gradient(to bottom, white ${(comparedUsers[0]?.percentage) ? comparedUsers[0]?.percentage : 0}, rgba(0,0,0,0) ${(comparedUsers[0]?.percentage) ? comparedUsers[0].percentage : 0})` }} ></div>
-                        <div className={styles.name}>{comparedUsers[0]?.name}</div>
-                        <div className={styles.total}>${comparedUsers[0]?.salary.toLocaleString('en-US')}</div>
-                        <div className={styles.difference}>{(comparedUsers[0]?.difference < 0) ? `-` : ``}${Math.abs(comparedUsers[0]?.difference).toLocaleString('en-US')}{(comparedUsers[0]?.difference < 0) ? <Arrow arrowColor={'#2F80ED'} direction={'down'} /> : (comparedUsers[0]?.difference > 0)? <Arrow arrowColor={'#2F80ED'} direction={'up'} />:''}</div>
+                        <div className={styles.head} style={{ backgroundImage: ` linear-gradient(to bottom, white ${(compareEmployees[0]?.percentage) ? compareEmployees[0]?.percentage : 0}, rgba(0,0,0,0) ${(compareEmployees[0]?.percentage) ? compareEmployees[0].percentage : 0})` }} ></div>
+                        <div className={styles.name}>{compareEmployees[0]?.name}</div>
+                        <div className={styles.total}>${compareEmployees[0]?.salary.toLocaleString('en-US')}</div>
+                        <div className={styles.difference}>{(compareEmployees[0]?.difference < 0) ? `-` : ``}${Math.abs(compareEmployees[0]?.difference).toLocaleString('en-US')}{(compareEmployees[0]?.difference < 0) ? <Arrow arrowColor={'#2F80ED'} direction={'down'} /> : (compareEmployees[0]?.difference > 0)? <Arrow arrowColor={'#2F80ED'} direction={'up'} />:''}</div>
                     </div>
 
                     <div className={styles.middle}>
@@ -37,10 +37,10 @@ export default function Comparison({ idsCheckedList }) {
                     </div>
 
                     <div className={`${styles.person} ${styles.two} ${styles.low}`}>
-                        <div className={styles.head} style={{ backgroundImage: ` linear-gradient(to bottom, white ${(comparedUsers[1]?.percentage) ? comparedUsers[1]?.percentage : 0}, rgba(0,0,0,0) ${(comparedUsers[1]?.percentage) ? comparedUsers[1].percentage : 0})` }} ></div>
-                        <div className={styles.name}>{comparedUsers[1]?.name}</div>
-                        <div className={styles.total}>${comparedUsers[1]?.salary.toLocaleString('en-US')}</div>
-                        <div className={styles.difference}>{(comparedUsers[1]?.difference < 0) ? `-` : ``}${Math.abs(comparedUsers[1]?.difference).toLocaleString('en-US')}{(comparedUsers[1]?.difference < 0) ? <Arrow arrowColor={'#F2994A'} direction={'down'} /> : (comparedUsers[1]?.difference > 0)? <Arrow arrowColor={'#F2994A'} direction={'up'} />:''}</div>
+                        <div className={styles.head} style={{ backgroundImage: ` linear-gradient(to bottom, white ${(compareEmployees[1]?.percentage) ? compareEmployees[1]?.percentage : 0}, rgba(0,0,0,0) ${(compareEmployees[1]?.percentage) ? compareEmployees[1].percentage : 0})` }} ></div>
+                        <div className={styles.name}>{compareEmployees[1]?.name}</div>
+                        <div className={styles.total}>${compareEmployees[1]?.salary.toLocaleString('en-US')}</div>
+                        <div className={styles.difference}>{(compareEmployees[1]?.difference < 0) ? `-` : ``}${Math.abs(compareEmployees[1]?.difference).toLocaleString('en-US')}{(compareEmployees[1]?.difference < 0) ? <Arrow arrowColor={'#F2994A'} direction={'down'} /> : (compareEmployees[1]?.difference > 0)? <Arrow arrowColor={'#F2994A'} direction={'up'} />:''}</div>
 
                     </div>
 
